f3545c2250613df362be111b5cfe9990e836f0ac
Lukonde Mwila <outlier.developer@gmail.com>
2022-03-01 16:02:35 +0200

    feature: added iac and tests for end points

--- src/controllers/orders_controller.js
+++ src/controllers/orders_controller.js
@@ -2,7 +2,9 @@ const get = (req, res, next) => {
   try {
     const orders = [
       { id: '1', productId: '1a', orderFor: 'Lukas Mwila' },
-      { id: '2', productId: '1b', orderFor: 'Sebastian Kapalu' }
+      { id: '2', productId: '1b', orderFor: 'Lewis Mwila' },
+      { id: '3', productId: '1c', orderFor: 'Lewis Mwila' },
+      { id: '4', productId: '1d', orderFor: 'Lukas Mwila' },
     ];
     res.status(200).send(orders);
   } catch (e) {
