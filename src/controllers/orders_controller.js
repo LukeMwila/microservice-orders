73f379620bbae499f3663eee3a0a9a8680345bdf
Lukonde Mwila <outlier.developer@gmail.com>
2022-03-04 12:12:36 +0200

    feat: added bruce banner order

--- src/controllers/orders_controller.js
+++ src/controllers/orders_controller.js
@@ -5,6 +5,7 @@ const get = (req, res, next) => {
       { id: '2', productId: '1b', orderFor: 'Steve Rogers' },
       { id: '3', productId: '1c', orderFor: 'Tony Stark' },
       { id: '4', productId: '1d', orderFor: 'Reed Richards' },
+      { id: '5', productId: '1d', orderFor: 'Bruce Banner' }
     ];
     res.status(200).send(orders);
   } catch (e) {
