76c0dacc2c0b10c6b1a074ab401d4247de4a4289
Lukonde Mwila <outlier.developer@gmail.com>
2022-03-12 12:13:54 +0200

    feat: updated orders

--- src/controllers/orders_controller.js
+++ src/controllers/orders_controller.js
@@ -1,10 +1,10 @@
 const get = (req, res, next) => {
   try {
     const orders = [
-      { id: '1', productId: '1a', orderFor: 'Peter Parker' },
+      { id: '1', productId: '1a', orderFor: 'Bruce Wayne' },
       { id: '2', productId: '1b', orderFor: 'Steve Rogers' },
       { id: '3', productId: '1c', orderFor: 'Tony Stark' },
-      { id: '4', productId: '1d', orderFor: 'Reed Richards' },
+      { id: '4', productId: '1d', orderFor: 'Natasha Romanoff' },
     ];
     res.status(200).send(orders);
   } catch (e) {
