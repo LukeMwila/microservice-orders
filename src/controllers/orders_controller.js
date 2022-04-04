a4089eff3ac1686bf2404e48d4e18cc6bfbb7926
Lukonde Mwila <outlier.developer@gmail.com>
2022-04-04 17:01:34 +0200

    feat: added new order

--- src/controllers/orders_controller.js
+++ src/controllers/orders_controller.js
@@ -6,7 +6,8 @@ const get = (req, res, next) => {
       { id: '3', productId: '1c', orderFor: 'Tony Stark', deliveryDate: '04/04/2022', deliveryAddress: 'Stark Manor, Malibu' },
       { id: '4', productId: '1d', orderFor: 'Lukonde Mwila', deliveryDate: '07/04/2022', deliveryAddress: 'Somewhere in Cape Town' },
       { id: '5', productId: '1d', orderFor: 'Bruce Banner', deliveryDate: '10/04/2022', deliveryAddress: 'Avengers Base, New York' },
-      { id: '6', productId: '1d', orderFor: 'Natasha Romanoff', deliveryDate: '03/04/2022', deliveryAddress: 'Avengers Base, New York' }
+      { id: '6', productId: '1d', orderFor: 'Natasha Romanoff', deliveryDate: '03/04/2022', deliveryAddress: 'Avengers Base, New York' },
+      { id: '7', productId: '1d', orderFor: 'Lu', deliveryDate: '03/04/2022', deliveryAddress: 'Dev Conf Cape Town' }
     ];
     res.status(200).send(orders);
   } catch (e) {
