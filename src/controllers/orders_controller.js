61b3f2043130b7d3929f54845c219380910f1d95
Lukonde Mwila <outlier.developer@gmail.com>
2022-04-07 15:54:36 +0200

    feat: bumped version and added order for Adam

--- src/controllers/orders_controller.js
+++ src/controllers/orders_controller.js
@@ -4,6 +4,7 @@ const get = (req, res, next) => {
       { id: '1', productId: '1a', orderFor: 'Peter Parker', deliveryDate: '04/04/2022', deliveryAddress: 'Happy Hogans Condo, New York' },
       { id: '2', productId: '1b', orderFor: 'Steve Rogers', deliveryDate: '06/04/2022', deliveryAddress: 'Avengers Base, New York' },
       { id: '3', productId: '1b', orderFor: 'Yatin Badal', deliveryDate: '06/04/2022', deliveryAddress: 'DevConf Cape Town' },
+      { id: '3', productId: '1b', orderFor: 'Adam', deliveryDate: '06/04/2022', deliveryAddress: 'DevConf Joburg' },
       { id: '4', productId: '1c', orderFor: 'Tony Stark', deliveryDate: '04/04/2022', deliveryAddress: 'Stark Manor, Malibu' },
       { id: '5', productId: '1d', orderFor: 'Lukonde Mwila', deliveryDate: '07/04/2022', deliveryAddress: 'Somewhere in Cape Town' },
       { id: '6', productId: '1d', orderFor: 'Bruce Banner', deliveryDate: '10/04/2022', deliveryAddress: 'Avengers Base, New York' },
