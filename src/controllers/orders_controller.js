fcf48b1277729dba58c8009ccc846c5675bae61c
Lukonde Mwila <outlier.developer@gmail.com>
2022-04-12 16:54:55 +0200

    feat: updated orders with new delivery addresses

--- src/controllers/orders_controller.js
+++ src/controllers/orders_controller.js
@@ -1,10 +1,11 @@
 const get = (req, res, next) => {
   try {
     const orders = [
-      { id: '1', productId: '1a', orderFor: 'Bruce Wayne' },
-      { id: '2', productId: '1b', orderFor: 'Steve Rogers' },
-      { id: '3', productId: '1c', orderFor: 'Tony Stark' },
-      { id: '4', productId: '1d', orderFor: 'Natasha Romanoff' },
+      { id: '1', productId: '1a', orderFor: 'Bruce Wayne', deliveryAddress: 'Wayne Manor' },
+      { id: '2', productId: '1b', orderFor: 'Steve Rogers', deliveryAddress: 'Avengers HQ' },
+      { id: '3', productId: '1c', orderFor: 'Tony Stark', deliveryAddress: 'Stark Tower' },
+      { id: '4', productId: '1d', orderFor: 'Natasha Romanoff', deliveryAddress: 'Avengers HQ' },
+      { id: '5', productId: '1d', orderFor: 'Clark Kent', deliveryAddress: 'The Daily Planet HQ' }
     ];
     res.status(200).send(orders);
   } catch (e) {
