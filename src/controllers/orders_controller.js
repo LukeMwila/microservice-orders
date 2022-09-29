5fd029f92c8cd094343d563c168f7b843f4950de
Lukonde Mwila <outlier.developer@gmail.com>
2022-09-29 17:09:50 +0200

    feat: added deliveryDate to orders api

--- src/controllers/orders_controller.js
+++ src/controllers/orders_controller.js
@@ -1,11 +1,11 @@
 const get = (req, res, next) => {
   try {
     const orders = [
-      { id: '1', productId: '1a', orderFor: 'Bruce Wayne', deliveryAddress: 'Wayne Manor' },
-      { id: '2', productId: '1b', orderFor: 'Steve Rogers', deliveryAddress: 'Avengers HQ' },
-      { id: '3', productId: '1c', orderFor: 'Tony Stark', deliveryAddress: 'Stark Tower' },
-      { id: '4', productId: '1d', orderFor: 'Natasha Romanoff', deliveryAddress: 'Avengers HQ' },
-      { id: '5', productId: '1d', orderFor: 'Clark Kent', deliveryAddress: 'The Daily Planet HQ' }
+      { id: '1', productId: '1a', orderFor: 'Bruce Wayne', deliveryAddress: 'Wayne Manor', deliveryDate: '02/11/2022' },
+      { id: '2', productId: '1b', orderFor: 'Steve Rogers', deliveryAddress: 'Avengers HQ', deliveryDate: '10/10/2022' },
+      { id: '3', productId: '1c', orderFor: 'Tony Stark', deliveryAddress: 'Stark Tower', deliveryDate: '02/10/2022' },
+      { id: '4', productId: '1d', orderFor: 'Natasha Romanoff', deliveryAddress: 'Avengers HQ', deliveryDate: '22/09/2022' },
+      { id: '5', productId: '1d', orderFor: 'Clark Kent', deliveryAddress: 'The Daily Planet HQ', deliveryDate: '12/12/2022' }
     ];
     res.status(200).send(orders);
   } catch (e) {
